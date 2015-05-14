f4726c078c63a4914b64340f1f97e5a082f674d0
Serge Zvenigorodsky <szvenig7@students.d125.org>
2015-05-14 15:27:13 -0500

    Changed MovingObj, started Runner.

--- MovingObj.java
+++ MovingObj.java
@@ -10,20 +10,16 @@ public abstract class MovingObj implements Locatable
     private Voxel referanceVoxel; //voxel from which obj is referanced/built
     private Voxel velocity; //Voxel type because velocity vector is drawn from (x,y,z,): (0,0,0) to the Voxel's coordiantes. //IS THIS VELOCITY INHERITED BY OTHER OBJS? AND ALL THE VELOCITY METHODS?
 
+    //CONSTRUCTOR that sets voxel/referancevoxel. Call constructor in children classes with super(referanceVoxel, velocity)
+    
     public Voxel getReferanceVoxel()
     {
         return referanceVoxel;
     }
-    public void setReferanceVoxel()
-    {
-        
-    }
+    public abstract void setReferanceVoxel();
     public Voxel getVelocity()
     {
         return velocity;
     }
-    public void setVelocity()
-    {
-        
-    }
+    public abstract void setVelocity();
 }
--- Runner.java
+++ Runner.java
@@ -7,27 +7,9 @@
  */
 public class Runner
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Runner
-     */
-    public Runner()
+    public static void main(String args[])
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        grid ThreeDGrid = new ThreeDGrid(100);
+        
     }
 }
