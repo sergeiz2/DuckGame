cc118d82f45bd5280737d79a09d6b5cc03c51dba
Serge Zvenigorodsky <szvenig7@students.d125.org>
2015-05-18 00:17:37 -0500

    wrote sequence of events as they should happen in comments in the Runner class.

--- Runner.java
+++ Runner.java
@@ -9,7 +9,25 @@ public class Runner
 {
     public static void main(String args[])
     {
-        
+        //Tells gui to display a menu with selections for a gun and ammunition)
+        //recieves gun params from gui
+        //recieves Shot params from gui
+        //calculates nOfPellets, weight overall, surface area of each pellet (for wind resistance)
+        //displays x-y view of game screen, with a button for exiting back to the menu. //will eventually be x-y-z
+        //don't draw the gun yet // will eventually draw a rectangle
+        //draw crosshairs instead of a mouse pointer
+        //generate a duck with a random position vector within u units of the gun, and a random velocity vector, generate a new duck at random times between every 5-20 seconds. 
+        //set the duck's z-velocity to zero // will eventually omit this
+        //wait for a mouseClicked event to fire.
+        //when the gun fires, depending on angle of the pointer, and the mass of each pellet, caluclate the initial shot velocity, and start a timer
+        //find the distance to the duck,
+        //set the shot's z-velocity to zero //will eventually omit this.
+        //every n nanoseconds, account for every force acting on the shot/duck and recalculate their locations. Repaint the board.
+        //until all locations in the shot have travelled d units past the distnace to the duck, record all positions of the duck and of the shot, sorted by time since the shot.
+        //reset the timer
+        //determine if there are any common points at the same time
+        //determine whether or not the duck will fall using probablility and the common points.
+        //repeat the process.
         
     }
 }
